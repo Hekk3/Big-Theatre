7dce368336ffe7d977e934a84aed5dbd8e2805eb
Hekk3 <timurh434@gmail.com>
2020-10-31 13:48:24 +0300

    Adaptive.

--- js/main.js
+++ js/main.js
@@ -1,58 +1,139 @@
-// mnu-burger
-let btnBurger = document.querySelector(".left-panel-icon");
-let closeBurger = document.querySelector('.close');
-let leftPanel = document.querySelector(".left-panel");
-
-btnBurger.addEventListener("click", function(e) {
-	leftPanel.classList.toggle('open');
-});
-
-closeBurger.addEventListener('click', function(e) {
-	leftPanel.classList.toggle('open');
-});
-// slider-one
-const buttonSlide = document.querySelector('.header-slider-arrow2');
-const buttonSlideLeft = document.querySelector('.header-slider-arrow1');
-const slide = document.querySelectorAll('.slide');
+// First Slider
+const prev = document.querySelector('.slider-hero__prev'),
+	  next = document.querySelector('.slider-hero__next'),
+	  slides = document.querySelectorAll('.slider-hero__slide');
 let counter = 0;
+let slidesLength = slides.length;
 
+// Default.
+slides[0].classList.add('slider-hero__slide--active');
 
-let slider = () => {
-	for (let i = 0; i < slide.length; i++) {
-		slide[i].classList.add('hide');
-	}
-	slide[counter].classList.remove('hide');
+if (counter == 0) {
+	// prev.classList.add("slider-hero__next--none");
 }
 
-buttonSlide.addEventListener("click", function() {
-	if (counter + 1 == slide.length) {
-		counter = 0;
+
+const move = function() {
+
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].classList.remove('slider-hero__slide--active');
+	}
+
+	slides[counter].classList.add('slider-hero__slide--active');
+
+	console.log(counter);
+}
+
+next.addEventListener('click', () => {
+	if (counter + 1 == slides.length) {
+		next.classList.add("slider-hero__next--none");
 	} else {
+		next.classList.remove("slider-hero__next--none");
 		counter++;
 	}
-	slider();
+	move();
 });
 
-buttonSlideLeft.addEventListener("click", function() {
-	if (counter - 1 == -1) {
-		counter = slide.length - 1;
+prev.addEventListener('click', () => {
+	if (counter === 0) {
+		prev.classList.add("slider-hero__next--none");
 	} else {
+		prev.classList.remove("slider-hero__next--none");
 		counter--;
 	}
-	slider();
+
+	move();
 });
 
 
-// slider-two-end
+// second slider
+// const secondPrev = document.querySelector('.staging__button-prev'),
+// 	  secondNext = document.querySelector('.staging__button-next'),
+// 	  item = document.querySelectorAll('.staging__item'),
+// 	  track = document.querySelector('.staging__track');
 
-let quotesBtn = document.querySelector('.project-slide-button');
-let quotesSlider = document.querySelector('.project-slides');
-let counterS = 0;
+// let position = 0;
+// let countItems = item.length;
+// let width = item[0].clientWidth;
 
-quotesBtn.addEventListener("click", function() {
-	counterS = counterS - 443;//486.8;
-	if (counterS < -1772) {
-		counterS = 0;
-	}
-	quotesSlider.style.left = counterS + "px";
-});
+
+// // Default.
+// track.style.width = width * countItems + "px";
+// secondPrev.style.pointerEvents = "none";
+
+
+// let checkSize = () => {
+// 	// check, when we need turn on the button(secondNext)
+// 	const trackCheck = track.getBoundingClientRect();
+// 	const doc = document.documentElement.getBoundingClientRect();
+// 	let total = doc.right - trackCheck.right;
+// 	return total;
+// }
+
+// let moveTrack = () => {
+// 	track.style.transform = "translateX(" + position + "px)"; // When we click on the button, we move the "track" using translateX + position.
+// }
+
+// let checkBtn = () => { // check buttons - check when the button need stops
+// 	position === 0 ? secondPrev.style.pointerEvents = "none" : secondPrev.style.pointerEvents = "auto" ;
+// 	position === -width * countItems ? secondNext.style.pointerEvents = "none" : secondNext.style.pointerEvents = "auto" ;
+// }
+
+// if (checkSize() !== 0 || checkSize() < 0) {
+// 	secondNext.addEventListener('click', () => {
+// 		if (checkSize() >= 0) {
+// 			secondNext.style.pointerEvents = "none";
+// 		} else {
+// 			position -= width; // take width the item;
+// 			moveTrack();
+// 			checkBtn();
+// 		}
+// 	});
+// } else {
+// 	secondNext.style.pointerEvents = "none";
+// }
+
+// secondPrev.addEventListener('click', () => {
+// 	position += width; // take width the item;
+
+// 	moveTrack();
+// 	checkBtn();
+// });
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: 'auto',
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
+
+
+// burger
+const close = document.querySelector('.close'),
+			burger = document.querySelector('.burger'),
+			nav = document.querySelector('.header__nav');
+
+
+burger.addEventListener('click', () => {
+	nav.classList.toggle('header__nav--active');
+})
+
+close.addEventListener('click', () => {
+	nav.classList.toggle('header__nav--active');
+})
